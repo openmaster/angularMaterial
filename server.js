6a2af12648749a51bad4566bda820e46eb08f70e
Vishavjeet Singh <vishav4@gmail.com>
2018-08-22 22:07:12 -0400

    heroku setup conpleted

---
+++ server.js
@@ -0,0 +1,14 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/angularMaterial'));
+//app.use('/*', express.static()
+
+
+// Start the app by listening on the default Heroku port
+//app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3200, () => console.log('Example app listening on port 3200!'));
